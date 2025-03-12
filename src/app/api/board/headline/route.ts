7a2930a38835fde608f5a7e31e4a9b64d4464ce5
juneh2633 <juneh2633@gmail.com>
2025-03-12 15:01:19 +0900

    feat:headline

---
+++ src/app/api/board/headline/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server';
+import { prisma } from '@/app/api/prisma';
+
+export async function GET(request: Request) {
+  const data = await prisma.board.findMany({
+    select: {
+      uid: true,
+      hasFile: true,
+      title: true,
+      date: true,
+    },
+    where: {
+      deletedAt: null,
+      isHeadline: 1,
+    },
+    orderBy: {
+      date: 'desc',
+    },
+  });
+  return NextResponse.json({
+    data,
+  });
+}
