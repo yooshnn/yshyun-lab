6ffc884ec07a8ab17a4b22b222336fbd0bcd8fd7
juneh2633 <juneh2633@gmail.com>
2025-03-12 22:46:50 +0900

    fix: board desc to asc

--- src/app/api/board/news/route.ts
+++ src/app/api/board/news/route.ts
@@ -1,22 +1,22 @@
-import { NextResponse } from 'next/server';
-import { prisma } from '@/app/api/prisma';
+import { NextResponse } from "next/server";
+import { prisma } from "@/app/api/prisma";
 
 export async function GET(request: Request) {
-  const data = await prisma.board.findMany({
-    select: {
-      uid: true,
-      hasFile: true,
-      title: true,
-      date: true,
-    },
-    where: {
-      deletedAt: null,
-    },
-    orderBy: {
-      date: 'desc',
-    },
-  });
-  return NextResponse.json({
-    data,
-  });
+    const data = await prisma.board.findMany({
+        select: {
+            uid: true,
+            hasFile: true,
+            title: true,
+            date: true,
+        },
+        where: {
+            deletedAt: null,
+        },
+        orderBy: {
+            date: "asc",
+        },
+    });
+    return NextResponse.json({
+        data,
+    });
 }
