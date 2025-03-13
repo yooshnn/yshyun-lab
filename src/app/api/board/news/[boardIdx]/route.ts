f3b251c0b8a1e321c87be558874cdd2abc37277c
juneh2633 <juneh2633@gmail.com>
2025-03-13 13:47:26 +0900

    feat: params promise

--- src/app/api/board/news/[boardIdx]/route.ts
+++ src/app/api/board/news/[boardIdx]/route.ts
@@ -1,8 +1,8 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { prisma } from "@/app/api/prisma";
 
-export async function GET(request: NextRequest, { params }: { params: { boardIdx: string } }) {
-    const { boardIdx } = params;
+export async function GET(request: Request, { params }: { params: Promise<{ boardIdx: string }> }) {
+    const { boardIdx } = await params;
     const uid = parseInt(boardIdx, 10);
     const data = await prisma.board.findUnique({
         select: {
@@ -20,5 +20,7 @@ export async function GET(request: NextRequest, { params }: { params: { boardIdx
     if (!data) {
         return NextResponse.json({ error: "no board data" }, { status: 404 });
     }
-    return NextResponse.json({ data });
+    return NextResponse.json({
+        data,
+    });
 }
